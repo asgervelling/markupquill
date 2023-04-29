8bbe5e9b4b2986586cf2ae4c0f39950dfcd20144
asgervelling <asger.skov.velling@gmail.com>
2023-04-29 20:29:20 +0200

    Split into more functions

--- app/markupquill/src/convert.py
+++ app/markupquill/src/convert.py
@@ -1,37 +1,14 @@
 from textwrap import dedent
 
 
-def matrix(m: list[list[str]]) -> str:
+def matrix(rows: list[list[str]]) -> str:
     """Generate LaTeX code for a matrix"""
+    return '\\begin{bmatrix}' + concat_rows(rows) + '\\end{bmatrix}'
+        
 
-    def elements(m):
-        rows = len(m)
-        match rows:
-            case 0:
-                return ''
-            case 1:
-                return ' & '.join(m[0])
-            case _:
-                return ' & '.join(m[0]) + ' \\\\ ' + elements(m[1:])
-
-    return '\\begin{bmatrix}' + elements(m) + '\\end{bmatrix}'
-
-
-def table(t: list[list[str]]) -> str:
+def table(rows: list[list[str]]) -> str:
     """Generate LaTeX code for a table"""
-    num_columns = max(map(len, t))
-
-    def table_rows(t, first_row=True):
-        rows = len(t)
-        match rows:
-            case 0:
-                return ''
-            case 1:
-                return f'{" & ".join(t[0])} \\\\'
-            case _:
-                line = '\\hline\n' if first_row else ''
-                return f'{" & ".join(t[0])} \\\\\n{line}{table_rows(t[1:], first_row=False)}'
-
+    num_columns = max(map(len, rows))
     borders = f'|{"l|" * num_columns}'
     start = dedent(
         f'''
@@ -46,4 +23,19 @@ def table(t: list[list[str]]) -> str:
         \\end{tabular}
         \\end{table}'''
     )
-    return f'{start}{table_rows(t)}{end}'
+    return f'{start}{table_header(rows[0])}{concat_rows(rows[1:])}{end}'
+
+
+def table_header(row: list[str]):
+    return f"{' & '.join(row)} \\\\\n\\hline\n"
+
+
+def concat_rows(rows: list[list[str]]) -> str:
+    """Joins a list with the string ' & '"""
+    match len(rows):
+        case 0:
+            return ''
+        case 1:
+            return ' & '.join(rows[0])
+        case _:
+            return f'{" & ".join(rows[0])} \\\\\n{concat_rows(rows[1:])}'
